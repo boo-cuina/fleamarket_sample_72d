e20a8ba86c7c5958bbd30d3fc7aba668007b6079
mamiemon22c <mamiemon22c@gmail.com>
2020-06-03 00:49:26 +0900

    setup for routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
     root to: 'items#index'
-    resources :items
-    
+    resources :items do
+      member do
+        get :confirm
+      end
+    end
+    resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
