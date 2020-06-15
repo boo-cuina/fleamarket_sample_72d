7b8ede9228bc96a6ed12457843106f01ce420768
kudoushinichiro <kudou.shinichiro@gmail.com>
2020-06-15 23:36:27 +0900

    photosテーブル・カラムの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200615141120) do
+ActiveRecord::Schema.define(version: 20200615143309) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "last_name",          null: false
@@ -65,6 +65,14 @@ ActiveRecord::Schema.define(version: 20200615141120) do
     t.index ["seller_id"], name: "index_items_on_seller_id", using: :btree
   end
 
+  create_table "photos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "image",      null: false
+    t.integer  "item_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_photos_on_item_id", using: :btree
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -89,4 +97,5 @@ ActiveRecord::Schema.define(version: 20200615141120) do
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users", column: "buyer_id"
   add_foreign_key "items", "users", column: "seller_id"
+  add_foreign_key "photos", "items"
 end
