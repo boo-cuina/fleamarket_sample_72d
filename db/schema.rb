138e8f05788177beb50df9aaec3b281baad8cd81
kudoushinichiro <kudou.shinichiro@gmail.com>
2020-06-15 22:38:16 +0900

    cardsテーブル・カラムの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200615083117) do
+ActiveRecord::Schema.define(version: 20200615130947) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "last_name",          null: false
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20200615083117) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
+  end
+
+  create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "card_token",  null: false
+    t.string   "customer_id", null: false
+    t.integer  "user_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_cards_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -49,4 +58,5 @@ ActiveRecord::Schema.define(version: 20200615083117) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "cards", "users"
 end
