ec3549af2bf3c9ac82b1c49de4ae1ac34aa22435
koji0316 <gorigori0316@gmail.com>
2020-06-19 00:58:21 +0900

    新規登録画面/ログイン画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def show
   end
+
+  def create
+    User.create(user_params)
+  end
   
+  private
+    def user_params
+      params.requie(:user).permit(:nick_name, :mail, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :postcode, :prefecture, :city, :block, :building)
+    end
+
 end
