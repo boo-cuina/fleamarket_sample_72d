9e0fed159689598b259b5a19fd3deb14fec6c7b3
koji0316 <gorigori0316@gmail.com>
2020-06-25 22:56:17 +0900

    新規登録画面/ログイン画面/controller記述削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create(user_params)
   end
   
-  private
-    def user_params
-      params.requie(:user).permit(:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_reading, :first_name_reading, :birthday)
-    end
-
-    def address_params
-      params.require(:user).require(:address).permit(:postcode, :prefecture, :city, :block, :building).merge(user_id: current_user.id)
-    end
-    
 
 end
