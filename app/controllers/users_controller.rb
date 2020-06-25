0a106eb79a2815655640c3a643a4d18821907c39
koji0316 <gorigori0316@gmail.com>
2020-06-25 22:43:10 +0900

    新規登録画面/ログイン画面/users_controller編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,12 @@ class UsersController < ApplicationController
   
   private
     def user_params
-      params.requie(:user).permit(:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_reading, :first_name_reading, :birthday, :postcode, :prefecture, :city, :block, :building)
+      params.requie(:user).permit(:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_reading, :first_name_reading, :birthday)
     end
+
+    def address_params
+      params.require(:user).require(:address).permit(:postcode, :prefecture, :city, :block, :building).merge(user_id: current_user.id)
+    end
+    
 
 end
