4674579b351e3a364a5c1ba70399af37771f9845
mamiemon22c <mamiemon22c@gmail.com>
2020-06-28 16:04:04 +0900

    routing, created controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  validates :nickname, :email, :password, :last_name, :last_name_reading, :first_name, :first_name_reading, :birthday, presence: true
+   
   has_many :items
   has_many :addresses
   has_one :card
