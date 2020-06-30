11e72c678f8ea5cf1db26acccb9d284bb3631c3c
mamiemon22c <mamiemon22c@gmail.com>
2020-06-30 11:05:54 +0900

    調整

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,7 +5,7 @@ class Address < ApplicationRecord
 
   validates :user, presence: true, foreign_key: true
 
-  with_options presence: true d o
+  with_options presence: true do
     validates :last_name
     validates :last_name_reading
     validates :first_name
