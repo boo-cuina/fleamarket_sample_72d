f7f79c80f80bff0e88ced559fdf7140fac12cc6c
kudoushinichiro <kudou.shinichiro@gmail.com>
2020-07-08 01:36:54 +0900

    微調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    @item.photos.build
+    @item.photos.new
   end
 
   def create
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,5 +6,5 @@ class Item < ApplicationRecord
   has_many :photos
   belongs_to :category
 
-  accepts_nested_attributes_for :photos
+  accepts_nested_attributes_for :photos, allow_destroy: true
 end
