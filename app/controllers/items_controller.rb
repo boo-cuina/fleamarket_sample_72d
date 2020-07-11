90f8f18e404dbb85c384b541c7ef48f7e2c07dca
koji0316 <gorigori0316@gmail.com>
2020-07-11 18:28:57 +0900

    商品購入機能-items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,4 +23,21 @@ class ItemsController < ApplicationController
 
   def confirm
   end
+
+  def pay
+    @item = Item.find(params[:id])
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    charge = Payjp::Charge.create(
+    amount: @item.price,
+    card: params['payjp-token'],
+    currency: 'jpy'
+    )
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :size, :status, :price, :shipping_fee, :shippingfrom_id, :shipping_days, :buyer_id, :category_id, :created_at, :updated_at).merge(user_id: current_user.id)
+  end
+
 end
