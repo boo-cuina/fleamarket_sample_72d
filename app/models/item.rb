e437692153b584d59566a04ebfe6fe91862dcc03
ararat0623 <kohei.aarai@gmail.com>
2020-07-11 22:26:28 +0900

    model_attributes_fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,6 @@ class Item < ApplicationRecord
   belongs_to :seller,class_name: 'User',foreign_key: 'seller_id'
   has_many :photos
   belongs_to :category
-  accepts_nested_attributes_for :photos
+  accepts_nested_attributes_for :photos,allow_destroy: true
   accepts_nested_attributes_for :category
 end
