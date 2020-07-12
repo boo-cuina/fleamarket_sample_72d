890137ac2052e9ae4ada51b465036828ea935ca1
kudoushinichiro <kudou.shinichiro@gmail.com>
2020-07-12 21:12:36 +0900

    itemモデルの微調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :shippingfrom
 
-  belongs_to :user, optional: true
+  belongs_to :buyer, class_name: 'User',foreign_key: 'buyer_id', optional: true
+  belongs_to :seller,class_name: 'User',foreign_key: 'seller_id', optional: true
   has_many :photos
   belongs_to :category, optional: true
 
