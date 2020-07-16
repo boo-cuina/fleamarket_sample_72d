605f545c2cf4ee327d0f0610688bf6c62adb08b3
mamiemon22c <mamiemon22c@gmail.com>
2020-07-16 12:20:07 +0900

    デバックコードの削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -28,10 +28,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
 
   def create_address
-    # binding.pry
     @user = User.new(session["devise.regist_data"]["user"])
     @address = Address.new(address_params)
-    # binding.pry
     unless @address.valid?
       flash.now[:alert] = @address.errors.full_messages
       render :new_address and return
