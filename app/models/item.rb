48d54f458e28193ed1b1f68d9e59fd26e7f50bce
gou555 <go_san_u@ybb.ne.jp>
2020-07-17 15:06:39 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id', optional: true
 
   has_many :photos
-  belongs_to :category
+  belongs_to :category, optional: true
 
   accepts_nested_attributes_for :photos, allow_destroy: true
 end
