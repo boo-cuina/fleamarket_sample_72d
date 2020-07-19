2f99a821e579abf4fe7b424eda37a21be29e419a
ararat0623 <kohei.aarai@gmail.com>
2020-07-19 15:44:07 +0900

    buyer_optional:true_add

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :shippingfrom
   
-  belongs_to :buyer, class_name: 'User',foreign_key: 'buyer_id'
+  belongs_to :buyer, class_name: 'User',foreign_key: 'buyer_id', optional: true
   belongs_to :seller,class_name: 'User',foreign_key: 'seller_id'
   has_many :photos
   belongs_to :category
