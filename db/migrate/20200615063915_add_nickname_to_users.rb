06dbcd9b20c4d7d8f85fabce120d202181c3e445
kudoushinichiro <kudou.shinichiro@gmail.com>
2020-07-19 17:59:00 +0900

    重複しているmigrationファイルの内容削除

--- db/migrate/20200615063915_add_nickname_to_users.rb
+++ db/migrate/20200615063915_add_nickname_to_users.rb
@@ -1,11 +1,4 @@
 class AddNicknameToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :nickname,           :string, null: false, unique: true
-    add_column :users, :phone_number,       :string, null: false, unique: true
-    add_column :users, :last_name,          :string, null: false
-    add_column :users, :last_name_reading,  :string, null: false
-    add_column :users, :first_name,         :string, null: false
-    add_column :users, :first_name_reading, :string, null: false
-    add_column :users, :birthday,           :date,   null: false
   end
 end
