d7d364f66d356b06fb3ab14579f52384cf597022
kudoushinichiro <kudou.shinichiro@gmail.com>
2020-07-24 15:59:05 +0900

    出品のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,15 +10,16 @@ class Item < ApplicationRecord
 
   accepts_nested_attributes_for :photos, allow_destroy: true
 
-  validates :name, presence: true
-  validates :description, presence: true
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :description, presence: true,  length: { maximum: 1000 }
   validates :size, presence: true
   validates :status, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :shipping_fee, presence: true
   validates :shippingfrom, presence: true
   validates :shipping_days, presence: true
   validates :seller, presence: true
   validates :category, presence: true
+  validates :photos, presence: true
 
 end
