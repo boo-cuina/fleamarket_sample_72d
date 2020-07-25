5e127c3ff52fc9eba25a55f4c286a21672d3be10
mamiemon22c <mamiemon22c@gmail.com>
2020-07-25 19:23:49 +0900

    不要なコメントアウトの削除

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,12 +6,10 @@ class Users::SessionsController < Devise::SessionsController
   # GET /resource/sign_in
   # def new
   #   super
-  #   binding.pry
   # end
 
   # POST /resource/sign_in
   # def create
-  #   binding.pry
   #   super
   # end
 
