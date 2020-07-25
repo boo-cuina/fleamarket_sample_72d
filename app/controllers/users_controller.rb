fee6faa0a518684edad56e87d4bb14690025e223
mamiemon22c <mamiemon22c@gmail.com>
2020-07-25 18:22:56 +0900

    session_destroyの追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
+  # def index
+  #   @users = User.all
+  # end
 
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
   end
 
-  def create
-    @user = User.new
-  end
+
+  # def create
+  #   @user = User.new
+  # end
   
 
 end
