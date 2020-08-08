7a617c9a4c667cddee07fff1670cdc6ef5a71d4b
boo-cuina <blau.himmel.cuina@gmail.com>
2020-08-09 02:51:00 +0900

    購入確認（controller）の実装
    confirmメソッドを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show,:edit,:destroy,:update]
-  before_action :move_to_index, only: [:edit,:destroy,:update]
-  before_action :set_category, only: [:new,:edit]
+  require 'payjp'
+  before_action :set_item, only: [:show, :edit, :destroy, :update, :confirm]
+  before_action :move_to_index, only: [:edit, :destroy, :update]
+  before_action :set_category, only: [:new, :edit]
   def get_category_children
     @category_children =  Category.find("#{params[:parent_name]}").children
   end
@@ -75,6 +76,40 @@ class ItemsController < ApplicationController
   end
 
   def confirm
+    #商品/ユーザー/クレジットカードの変数設定
+    @user = current_user
+    @first_photo = @item.photos[0]
+    @card = Card.where(user_id: current_user.id).first
+    @address = Address.where(user_id: current_user.id).first
+    #Payjpの秘密鍵を取得
+    Payjp.api_key =  ENV["PAYJP_PRIVATE_KEY"]
+    #Payjpから顧客情報を取得し、表示
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    #カスタマー情報からカードの情報を引き出す
+    @default_card_information = customer.cards.retrieve(customer.default_card)
+
+    #カードのアイコン表示のための定義づけ
+    @card_brand = @default_card_information.brand
+    case @card_brand
+    when "Visa"
+      @card_src = "logo_download_ph001.png"
+    when "JCB"
+      @card_src = "jcb-logomark-img-03.gif"
+    when "MasterCard"
+      @card_src = "logo_download_ph002.png"
+    when "American Express"
+      @card_src = "amex-logomark-img-06.gif"
+    when "Diners Club"
+      @card_src = "logo_download_ph003.png"
+    when "Discover"
+      @card_src = "discover-logomark-img-06.gif"
+    end
+    # viewの記述を簡略化
+    @last4 = @default_card_information.last4
+    ## 有効期限'月'を定義
+    @exp_month = @default_card_information.exp_month.to_s.rjust(2, '0')
+    ## 有効期限'年'を定義
+    @exp_year = @default_card_information.exp_year.to_s.slice(2, 2)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
         get 'get_category_grandchildren', defaults: { format: 'json' }
       end
       member do
-        get :confirm
+        get 'confirm'
         get 'get_category_children', defaults: { format: 'json' }
         get 'get_category_grandchildren', defaults: { format: 'json' }
       end
