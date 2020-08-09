5d242fb4ad845b3051adabbe6d1cd5a8eb5ea405
boo-cuina <blau.himmel.cuina@gmail.com>
2020-08-09 22:08:27 +0900

    コントローラー記載(purchaseとdone)
    購入をpurchaseで、購入完了をdoneで設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   require 'payjp'
-  before_action :set_item, only: [:show, :edit, :destroy, :update, :confirm]
+  before_action :set_item, only: [:show, :edit, :destroy, :update, :confirm, :purchase]
   before_action :move_to_index, only: [:edit, :destroy, :update]
   before_action :set_category, only: [:new, :edit]
   def get_category_children
@@ -81,35 +81,54 @@ class ItemsController < ApplicationController
     @first_photo = @item.photos[0]
     @card = Card.where(user_id: current_user.id).first
     @address = Address.where(user_id: current_user.id).first
-    #Payjpの秘密鍵を取得
-    Payjp.api_key =  ENV["PAYJP_PRIVATE_KEY"]
-    #Payjpから顧客情報を取得し、表示
-    customer = Payjp::Customer.retrieve(@card.customer_id)
-    #カスタマー情報からカードの情報を引き出す
-    @default_card_information = customer.cards.retrieve(customer.default_card)
+    if @card.blank?
+      redirect_to controller: "cards", action: "new"
+    else
+      #Payjpの秘密鍵を取得
+      Payjp.api_key =  ENV["PAYJP_PRIVATE_KEY"]
+      #Payjpから顧客情報を取得し、表示
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      #カスタマー情報からカードの情報を引き出す
+      @default_card_information = customer.cards.retrieve(customer.default_card)
 
-    #カードのアイコン表示のための定義づけ
-    @card_brand = @default_card_information.brand
-    case @card_brand
-    when "Visa"
-      @card_src = "logo_download_ph001.png"
-    when "JCB"
-      @card_src = "jcb-logomark-img-03.gif"
-    when "MasterCard"
-      @card_src = "logo_download_ph002.png"
-    when "American Express"
-      @card_src = "amex-logomark-img-06.gif"
-    when "Diners Club"
-      @card_src = "logo_download_ph003.png"
-    when "Discover"
-      @card_src = "discover-logomark-img-06.gif"
+      #カードのアイコン表示のための定義づけ
+      @card_brand = @default_card_information.brand
+      case @card_brand
+      when "Visa"
+        @card_src = "logo_download_ph001.png"
+      when "JCB"
+        @card_src = "jcb-logomark-img-03.gif"
+      when "MasterCard"
+        @card_src = "logo_download_ph002.png"
+      when "American Express"
+        @card_src = "amex-logomark-img-06.gif"
+      when "Diners Club"
+        @card_src = "logo_download_ph003.png"
+      when "Discover"
+        @card_src = "discover-logomark-img-06.gif"
+      end
+      # viewの記述を簡略化
+      @last4 = @default_card_information.last4
+      ## 有効期限'月'を定義
+      @exp_month = @default_card_information.exp_month.to_s.rjust(2, '0')
+      ## 有効期限'年'を定義
+      @exp_year = @default_card_information.exp_year.to_s.slice(2, 2)
     end
-    # viewの記述を簡略化
-    @last4 = @default_card_information.last4
-    ## 有効期限'月'を定義
-    @exp_month = @default_card_information.exp_month.to_s.rjust(2, '0')
-    ## 有効期限'年'を定義
-    @exp_year = @default_card_information.exp_year.to_s.slice(2, 2)
+  end
+
+  def purchase
+    @card = Card.where(user_id: current_user.id).first
+    #Payjpの秘密鍵を取得と決済処理
+    Payjp.api_key =  ENV["PAYJP_PRIVATE_KEY"]
+    charge = Payjp::Charge.create(
+      amount: @item.price, #支払金額を入力
+      customer: Payjp::Customer.retrieve(@card.customer_id), #顧客ID
+      currency: 'jpy'  #日本円
+    )
+    #製品のbuyer_idを付与
+    @item_buyer = Item.find(params[:id])
+    @item_buyer.update(buyer_id: current_user.id)
+    redirect_to done_item_path
   end
 
   private
