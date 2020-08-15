8522eb7ee1683a0636bd57b3def6fc96640f8e24
mamiemon22c <mamiemon22c@gmail.com>
2020-08-15 13:39:18 +0900

    正規表現の追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -6,10 +6,10 @@ class Address < ApplicationRecord
   # validates :user, presence: true
 
   with_options presence: true do
-    validates :last_name
-    validates :last_name_reading
-    validates :first_name
-    validates :first_name_reading
+    validates :last_name, format: {with: /\A[一-龥ぁ-ん]/}
+    validates :last_name_reading, format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name, format: {with: /\A[一-龥ぁ-ん]/}
+    validates :first_name_reading, format: {with: /\A[ァ-ヶー－]+\z/}
     validates :postcode, format: {with: /\A\d{3}-\d{4}\z/}
     validates :prefecture_id
     validates :city
